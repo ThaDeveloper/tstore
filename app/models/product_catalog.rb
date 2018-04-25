8e96483a036e5ba4a92b95f39ed75dadee02149b
Justin Ndwiga <ndwigajustin@gmail.com>
2018-04-25 23:24:41 +0300

    [featue] model test cases

--- app/models/product_catalog.rb
+++ app/models/product_catalog.rb
@@ -1,2 +1,6 @@
 class ProductCatalog < ApplicationRecord
+    validates :product_code, presence: true
+    validates :product_name, presence: true
+    validates :quantities, presence: true
+    validates :price, presence: true
 end
--- test/models/product_catalog_test.rb
+++ test/models/product_catalog_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class ProductCatalogTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Product validation should trigger" do
+    assert_not Product.new(product_name: "product name",
+      quantity: 12, price: 19.00).save
+    end
+
+    test "Product should save" do
+      assert Product.new(product_code: "PDC1", product_name: "product name",
+        quantity: 12, price: 19.00).save
+      end
 end
