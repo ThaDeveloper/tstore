9208cb436c99ea62120722dd2507c5f88b621be9
Justin Ndwiga <ndwigajustin@gmail.com>
2018-05-01 18:19:26 +0300

    Print receipt functionality

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -5,13 +5,17 @@ class Sale < ApplicationRecord
 
   validates :sale_code, :presence => {:message => 'is required, Sale not saved'}
   validates :items, :presence => {:message => '- atleast one item is required, Sale not saved'}
-  before_save :total_all    
+  before_save :subtotals, :total_all 
+
   include PublicActivity::Model  
   tracked owner: ->(controller, model) { controller && controller.current_user} 
   
+  #Map through the subtotal function for each individual item
   def subtotals  
     self.items.map { |i| i.subtotal }  
   end  
+
+  #Add together the subtotals
   def total_all  
     subtotals.sum  
   end  
